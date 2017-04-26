39bb98a2969813e141ce61a491caa6bcfe759bbe
Tse Kit Yam <me@kytse.com>
2017-04-26 23:14:59 +0800

    Use atan2() to calculate tangent

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -60,7 +60,7 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   }
   
   z_pred(0) = c2;
-  z_pred(1) = atan(py/px);
+  z_pred(1) = atan2(py, px);
   z_pred(2) = c3/c2;
   
   VectorXd y = z - z_pred;
